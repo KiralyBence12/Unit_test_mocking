6125253d083ebcbc3851cca676b9e4ae401c9bfd
KiralyBence12 <118981185+KiralyBence12@users.noreply.github.com>
2026-01-05 12:21:13 +0100

    a

--- test_simple_calculator_with_mock.py
+++ test_simple_calculator_with_mock.py
@@ -1,8 +1,30 @@
+import pytest
+from simple_calculator import SimpleCalculator
+from unittest.mock import Mock
+
 def test_add_with_mock_history_service():
-    pass
+    mock_history = Mock()
+    
+    calc = SimpleCalculator(history_service=mock_history)
+
+    result = calc.add(5, 3)
+
+    assert result == 8
+
+    mock_history.save_operation.assert_called_once()
+    mock_history.save_operation("add", 5, 3, 8)
 
 def test_multiply_with_multiple_mocks():
-    pass
+    mock_history = Mock()
+    
+    calc = SimpleCalculator(history_service=mock_history)
+
+    result = calc.multiply(5, 3)
+
+    assert result == 15
+
+    mock_history.save_operation.assert_called_once()
+    mock_history.save_operation("multiply", 5, 3, 15)
 
 def test_get_history_count_with_mock():
     pass
